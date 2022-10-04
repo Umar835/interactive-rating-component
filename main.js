d40d83ba38349d54e642757ed036b3ab5680096d
Umar Hussain <umarhussain771@gmail.com>
2022-10-04 12:36:46 +0100

    adds comments for user understanding

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ const submitBtn = document.querySelector('.submit');
 //get rating container
 const ratingContainer = document.querySelector('.container')
 
-// selsect thank you card
+// select thank you card
 const thanksContainer = document.querySelector('.thank-you');
 const thankYouPara = document.querySelector('.thank-you-para1');
 
+//toggles thank you component.
 const ratingNum = document.getElementById('rating')
 
  submitBtn.addEventListener('click', function(){
    thanksContainer.classList.remove('hidden')
    ratingContainer.style.display = 'none' 
  })
-
+//adds rating to paragraph in thank you component
 allButtons.forEach((num) =>{
      num.addEventListener('click', function(){
        const buttonNum = num.innerText
